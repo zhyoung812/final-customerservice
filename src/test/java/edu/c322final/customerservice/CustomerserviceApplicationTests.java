07916f4062f52aba6da9a1f3f94b1d969e75dc7a
Zachary Young <zhyoung@iu.edu>
2023-04-14 16:11:10 -0400

    Initial commit

---
+++ src/test/java/edu/c322final/customerservice/CustomerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.c322final.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
