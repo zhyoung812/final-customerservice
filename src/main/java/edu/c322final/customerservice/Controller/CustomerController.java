47acdc7b7207bb7f3018606c1bb40a7b568d637c
Zachary Young <zhyoung@iu.edu>
2023-04-21 16:26:44 -0400

    Added cors validation

--- src/main/java/edu/c322final/customerservice/Controller/CustomerController.java
+++ src/main/java/edu/c322final/customerservice/Controller/CustomerController.java
@@ -15,25 +15,25 @@ public class CustomerController {
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{id}")
     public Customer getCustomerById(@PathVariable int id) {
         return customerRepository.getReferenceById(id);
     }
 
 
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public List<Customer> findAllCustomers() {
         return customerRepository.findAll();
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping
     public int createCustomer(@RequestBody Customer customer) {
         Customer newCustomer = customerRepository.save(customer);
         return newCustomer.getId();
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/{id}")
     public void deleteCustomer(@PathVariable int id) {
         customerRepository.deleteById(id);
